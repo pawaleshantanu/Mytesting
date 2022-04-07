f3b493d978eadbb6a703ee47346a7b9b5fab15a1
Commercial Preceptor <Commercial Preceptor@DESKTOP-8KJSCHI>
2022-04-07 18:48:01 +0530

    First project''''''''

--- src/test.java
+++ src/test.java
@@ -1,9 +1,29 @@
 
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		public static void main(String[] args) {System.out.println("Series no are:");
+		int i=0;
+		int First=0;
+		int second=1;
+		int next;
+		while(i<=10) {
+		next=First+second;
+		First=second;
+		second=next;
+		
+		
+					System.out.print(First+",");
+					i++;
+					
+				}
+			}
+
+	
+
+
+
 
 	}
 
-}
+
